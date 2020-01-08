11a5ef089a1557eda1c6fef3a9d880cc4bcf4520
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2020-01-07 23:35:58 -0300

    DateInput component and SelectDate page logic

--- mobile/src/components/DateInput/index.android.js
+++ mobile/src/components/DateInput/index.android.js
@@ -1,5 +1,7 @@
 import React, { useMemo } from 'react';
 import { DatePickerAndroid } from 'react-native';
+import PropTypes from 'prop-types';
+
 import { format } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 
@@ -7,7 +9,7 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, DateButton, DateText } from './styles';
 
-export default function DateInput({ date, onChange }) {
+function DateInput({ date, onChange }) {
   const dateFormatted = useMemo(
     () => format(date, "dd 'de' MMMM 'de' yyyy", { locale: pt }),
     [date]
@@ -35,3 +37,10 @@ export default function DateInput({ date, onChange }) {
     </Container>
   );
 }
+
+DateInput.propTypes = {
+  date: PropTypes.instanceOf(Date).isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default DateInput;
